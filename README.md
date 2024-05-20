024ef8ef34ad92dcdfa3758c91082a8feff890f3
Jonny Saykosy <josayko@pm.me>
2024-05-20 16:13:45 +0200

    feat: MongoDB replicaSet with single node and TLS encryption

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dev-mongodb
+
+## Get started
+### Prerequisites
+`docker`, `docker compose`, `openssl`
+
+## Connection URI
+```
+mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/?replicaSet=rs&ssl=true&tlsAllowInvalidCertificates=true
+```
+
+## CLI connection with `mongosh`
+```shell
+mongosh --host rs/localhost  --port 27017 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --tls --tlsAllowInvalidCertificates
+```
+
+## Good to know
+- `--tlsAllowInvalidCertificates` is necessary because of self-signed TLS certificates
+- `security.keyFile` is required when authorization is enabled with replica sets
+- By default, `MONGO_INITDB_ROOT_USERNAME`:`MONGO_INITDB_ROOT_PASSWORD` = `admin:admin`
--- setup.sh
+++ setup.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-mkdir -p pki certificates
+mkdir -p pki tls
+
+echo "> Generate keyfile..."
 openssl rand -base64 756 > ${PWD}/pki/rs_keyfile
 chmod 0600 ${PWD}/pki/rs_keyfile
+echo "> Keyfile done!"
+
+echo "> Generate CA File..."
+openssl req -nodes -out tls/ca.pem \
+  -new -x509 -keyout tls/ca.key \
+  -subj "/C=FR/ST=Paris/L=Paris/O=josayko/OU=IT/CN=josayko.com/emailAddress=josayko@pm.me" \
+  -quiet
+echo "> CA File done!"
+
+echo "> Generate Certificates..."
+openssl req -nodes -newkey rsa:4096 -sha256 -keyout tls/mongodb_node_1.key \
+  -out tls/mongodb_node_1.csr \
+  -subj "/C=JP/ST=Tokyo/L=Tokyo/O=josayko/OU=MongoDB/CN=localhost/emailAddress=josayko@pm.me"
+echo "> Certificates done!"
+
+echo "> Signing Certificates..."
+openssl x509 -req -in tls/mongodb_node_1.csr -CA tls/ca.pem -CAkey tls/ca.key \
+  -set_serial 00 -out tls/mongodb_node_1.crt
+echo "> Certificates signing done!"
+
+# Concatenate each Certificate with its Key
+cat tls/mongodb_node_1.key tls/mongodb_node_1.crt > tls/mongodb_node_1.pem
 
 docker compose up -d
 
